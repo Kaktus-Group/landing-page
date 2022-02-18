7949cacedd02ed191a0ffe88904d13455d2c905d
javohirToshmurodov <rustamnuraliyev1@mail.ru>
2022-02-18 11:40:36 +0500

    route added components build

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,20 +1,38 @@
 import React from "react";
 import { navbar } from "../../utils/data";
 import { NavLink } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <div className="w-full px-6 py-4 bg-slate-500 flex">
+    <div className="w-full px-6 py-4 bg-slate-500 flex items-center gap-9 ">
       <div className="flex items-center">
         <h1 className="text-white text-3xl font-poppins font-bold">ZONE</h1>
         <span className="bg-btnPrimary p-1 mt-4 rounded-full"></span>
       </div>
-      <div className="links">
-        <ul className="flex gap-2">{
-          navbar.map(({id,title,path})=>(
-            <li>
-              <NavLink to={path} key={id}>{title}</NavLink>
-            </li>
-          ))}</ul>
+      <div className="links ">
+        <ul className="flex gap-2 font-poppins text-white">
+          <li className="mx-3">
+            <a href="#">Home</a>
+          </li>
+          <li className="mx-3">
+            <a href="#">Components</a>
+          </li>
+          <li className="mx-3">
+            <a href="#">Documentation</a>
+          </li>
+          <li className="mx-3">
+            Pages
+            <ul className="menu hidden">
+              <li>
+                {navbar.map(({ id, path, title }) => (
+                  <NavLink to={path} key={id}>
+                    {title}
+                  </NavLink>
+                ))}
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
       <div className="menu-append"></div>
     </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -3,38 +3,10 @@ import BlogPost from "../pages/BlogPost";
 import BlogPosts from "../pages/BlogPosts";
 import CheckOut from "../pages/Checkout";
 import CheckOutComplete from "../pages/CheckoutComplete";
-import Components from "../pages/Components";
 import Contact from "../pages/Contact";
-import Home from "../pages/Home";
 import Tour from "../pages/Tour";
 import Tours from "../pages/Tours";
-import Documentation from "../pages/Documentation";
-import MainPages from "../pages/MainPages";
 export const navbar = [
-  {
-    id: 1,
-    title: "Home",
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    id: 2,
-    title: "Components",
-    path: "/components",
-    element: <Components />,
-  },
-  {
-    id: 3,
-    title: "Documentation",
-    path: "/documentation",
-    element: <Documentation />,
-  },
-  {
-    id: 4,
-    title: "Pages",
-    path: "/",
-    element: <MainPages />,
-    drop: [
       {
         id: 1,
         title: "About",
@@ -83,6 +55,4 @@ export const navbar = [
         path: "/tours",
         element: <Tours />,
       },
-    ],
-  },
 ];
